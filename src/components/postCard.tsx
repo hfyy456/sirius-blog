447c5dd54cf749fbbd065158d6b5d86c2b08e575
hfyy456 <540363975@qq.com>
2020-11-16 17:49:47 +0800

    updated: fixed some styles

--- src/components/postCard.tsx
+++ src/components/postCard.tsx
@@ -5,14 +5,25 @@ interface Iprops {
   title: string,
   cover: string,
   date: string,
-  tags: Array<string>
+  tags: Array<string>,
+  preview: string,
 }
 const PostCard: React.FC<Iprops> = (props) => {
-  const { title, cover, date, tags } = props
+  const { title, cover, date, tags, preview } = props
   return (
     <div className="card-container">
-      <div className='card-cover'></div>
-      <div className='card-info'></div>
+      <div className='card-cover'>
+        <a href='www.baidu.com'>
+          <img alt='cover' src={cover} />
+
+        </a>
+      </div>
+      <div className='card-info'>
+        <h3 className='arti-title'>{title}</h3>
+        <section className='arti-preview'>{preview}</section>
+        <div className='arti-date'>{date}</div>
+        <div className='arti-tags'>{tags}</div>
+      </div>
     </div>
   )
 }
--- src/containers/HomePage.tsx
+++ src/containers/HomePage.tsx
@@ -5,7 +5,12 @@ export default function Home() {
   return (
     <main className='home-page'>
       <div className='post-list'>
-        <PostCard title='f' cover='s' date='s' tags={[]} />
+        <PostCard 
+          title='代码高亮和表格效果测试'
+          cover='https://cdn.jsdelivr.net/gh/jerryc127/CDN/img/butterfly-docs-01-cover.png'
+          date='Sep 14, 2019'
+          preview='最近「延禧攻略」大火，于是我也追了一波风。不得不说，服装配饰太精致太好看啦，精致到难以描绘(>_<)'
+          tags={[]} />
       </div>
     </main>
   )
