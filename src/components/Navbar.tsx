84a86327fa959993329047f42a0b1fb7a7db35f3
hfyy456 <540363975@qq.com>
2020-11-26 17:02:37 +0800

    updated：navbar expand and fold

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Navbar.scss'
 import { NavLink, useHistory } from 'react-router-dom'
 
 export default function Navbar(): JSX.Element {
   const path: any = useHistory()
+  const [isFirstLoad, setIsFirstLoad] = useState(true)
+  const [isExpansion, setIsExpansion] = useState(false);
   const blogName: string = 'Sirius Blog'
   const menuList: Array<any> = [{
     name: '首页',
@@ -17,21 +19,39 @@ export default function Navbar(): JSX.Element {
   useEffect(() => {
     console.log(path.location.pathname)
   })
+  const handClickCloseMenu = (e: any) => {
+    setIsExpansion(false)
+    document.body.style.overflowY='auto';
+
+  }
+  const handClickOpenMenu = (e: any) => {
+    setIsExpansion(true)
+    setIsFirstLoad(false)
+    document.body.style.overflowY='hidden';
+
+  }
   return (
-    <nav className="nav-container" >
-      <div className='top-menu'>
-        <span className='blog-name'>{blogName}</span>
+    <div className='nav-wrapper'>
+      <div className={`top-menu${isExpansion ? ' change' : ''}`}>
+        <span className='blog-name '>{blogName}</span>
         <span className='nav-actions'>
-          <span><i className='iconfont iconclose' /></span>
+          {isExpansion ?
+            <div className='icon-outer' onClick={handClickCloseMenu}><i className='iconfont iconclose' /></div>
+            : <div className='icon-outer' onClick={handClickOpenMenu}><i className='iconfont iconcategory-fill' /></div>
+          }
         </span>
       </div>
-      <div className='menus'>
-        {
-          menuList.map((item, index) => {
-            return <span className='menu-item' key={index}><NavLink activeClassName='menu-item-active' to={item.path}><i className={`iconfont icon${item.icon}-fill`} />{item.name}</NavLink></span>
-          })
-        }
-      </div>
-    </nav>
+      <nav className={`nav-container${isExpansion ? ' expand' : ''}${!isExpansion && !isFirstLoad ? ' fold' : ''}`} >
+        {isExpansion ? <div className='menus'>
+          {
+            menuList.map((item, index) => {
+              return <span className='menu-item' key={index}><NavLink activeClassName='menu-item-active' to={item.path}><i className={`iconfont icon${item.icon}-fill`} />{item.name}</NavLink></span>
+            })
+          }
+        </div> : ''}
+
+      </nav>
+    </div>
+
   )
 }
