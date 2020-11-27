fc1cd077d85fe959a32a6fdec0d4747dd5085052
hfyy456 <540363975@qq.com>
2020-11-27 19:33:51 +0800

    updated：fix some css.

--- src/containers/ArticlePage.tsx
+++ src/containers/ArticlePage.tsx
@@ -4,14 +4,20 @@ import Topbar from '../components/Topbar'
 import './Article.scss'
 export default function Article() {
   const converter = new showdown.Converter();
-  const text = '# hello, markdown!'
+  const text = '# hello, markdown!   hello, markdown!'
   const html = converter.makeHtml(text);
   return (
     <div className='page'>
       <Topbar />
       <main className='article-container'>
-        <article dangerouslySetInnerHTML={{ __html: html }}>
-        </article>
+        <div id="post">
+          <div className='infor'>
+            <h1>sdas</h1>
+            <div className='date'>Sep 14, 2019</div>
+          </div>
+          <article dangerouslySetInnerHTML={{ __html: html }}>
+          </article>
+        </div>
       </main>
     </div>
   )
